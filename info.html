a2853b85cb11c23d03a5e98a126b27fe9d67cc4d
ifotn <rich@infrontofthenet.com>
2020-01-09 19:08:52 -0500

    Lesson 1 files

---
+++ info.html
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<?php
+// use the built-in phpinfo function to get server & client info
+phpinfo();
+?>
+
+
+</body>
+</html>
---
+++ variables.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<?php
+
+// set 3 variables (always starting with a $)
+$x = 3;
+$y = 4;
+$z = $x + $y;
+
+echo $z;
+
+$x = 'abc';
+$y = 'def';
+$z = $x . $y;
+
+echo $z;
+?>
+
+</body>
+</html>
